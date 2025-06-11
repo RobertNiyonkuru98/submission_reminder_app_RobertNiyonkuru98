2f4a683e166c3930526aa758a49e4f37f4f5120d
RobertNiyonkuru98 <r.niyonkuru@alustudent.com>
2025-06-11 19:45:11 +0200

    Clean

--- create_environment.sh
+++ create_environment.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
 # Ask the user to enter the name
-
-read -p "Enter your directory name: " name
+echo "The directory has to have a name"
+read -p "Please,Enter your directory name: " name
 
 # Creating the folder
 
 dir="submission_reminder_$name"
 mkdir -p "$dir"
+
+# Creating subdirectories and files
+
+mkdir -p "$dir/app"
+mkdir -p "$dir/modules"
+mkdir -p "$dir/assets"
+mkdir -p "$dir/config"
+mv reminder.sh "$dir/app/"
+mv functions.sh "$dir/modules"
+mv submissions.txt "$dir/assets"
+mv config.env "$dir/config"
+touch $dir/startup.sh
+
+# Making them executable
+
+chmod +x "$dir/app/reminder.sh"
+chmod +x "$dir/modules/functions.sh"
+chmod +x "$dir/assets/submissions.txt"
+chmod +x "$dir/config/config.env"
+chmod +x "$dir/startup.sh"
+
+# Tell the user that we're done
+
+echo "The setup is complete."
+echo "You can check: $dir"
